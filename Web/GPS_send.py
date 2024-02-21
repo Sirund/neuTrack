993ea7e54589ab02a4ea88589c33229d2006449a
Pelangi Masita <121101474+angiebow@users.noreply.github.com>
2024-02-21 22:58:28 +0700

    Update GPS_send.py

--- Web/GPS_send.py
+++ Web/GPS_send.py
@@ -2,15 +2,26 @@ import pyrebase
 import serial
 import pynmea2
 
-firebaseConfig={
-  "apiKey": "AIzaSyArU2hVkqA7CCj00t26YzxSjyYx4TdmLs0",
-  "authDomain": "gps-tracker2-50503.firebaseapp.com",
-  "databaseURL": "https://gps-tracker2-50503-default-rtdb.firebaseio.com",
-  "projectId": "gps-tracker2-50503",
-  "storageBucket": "gps-tracker2-50503.appspot.com",
-  "messagingSenderId": "118394749599",
-  "appId": "1:118394749599:web:359b51af670a0d4e2ad8aa"
-    }
+# firebaseConfig={
+#   "apiKey": "AIzaSyArU2hVkqA7CCj00t26YzxSjyYx4TdmLs0",
+#   "authDomain": "gps-tracker2-50503.firebaseapp.com",
+#   "databaseURL": "https://gps-tracker2-50503-default-rtdb.firebaseio.com",
+#   "projectId": "gps-tracker2-50503",
+#   "storageBucket": "gps-tracker2-50503.appspot.com",
+#   "messagingSenderId": "118394749599",
+#   "appId": "1:118394749599:web:359b51af670a0d4e2ad8aa"
+#     }
+
+firebaseConfig = {
+  "apiKey": "AIzaSyCY4A7oLi4YiPVMV1VLOw2bHv0487Ww9FU",
+  "authDomain": "gps5-1ffe7.firebaseapp.com",
+  "projectId": "gps5-1ffe7",
+  "storageBucket": "gps5-1ffe7.appspot.com",
+  "messagingSenderId": "1057275623362",
+ "appId": "1:1057275623362:web:6fa154dcc3df768c41485b",
+  "measurementId": "G-7NBKMLJHS8",
+ "databaseURL": "https://gps5-1ffe7-default-rtdb.firebaseio.com"
+}
 
 firebase=pyrebase.initialize_app(firebaseConfig)
 db=firebase.database()
